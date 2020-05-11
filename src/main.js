b45aa6ca8eb1c00ff173f005f643ef371238cb02
tianhaoAA <1334750953@qq.com>
2020-05-11 19:55:53 +0800

    资源准备

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Element from 'element-ui'
-
+import '@/style/index.less'
 Vue.config.productionTip = false
 Vue.use(Element)
 
