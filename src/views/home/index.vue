daede907931668546ed0e23bbc81477c0ec01263
tianhaoAA <1334750953@qq.com>
2020-05-13 11:11:50 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,6 +5,11 @@
 <el-aside>
   <h1>左侧导航</h1>
 </el-aside>
+<el-container>
+  <el-header>头部</el-header>
+  <!-- 这里应该放置2级路由容器 -->
+  <el-mian>主要区域</el-mian>
+</el-container>
 </el-container>
 </template>
 
